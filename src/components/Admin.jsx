b6035dc777a47d29798e5c27afcb2a32f89d5b9b
Supragya2912 <anandsupragya@gmail.com>
2024-05-08 00:55:12 +0530

    listing of user and cabs on admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,29 +1,196 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Navbar from '../custom/Navbar'
-import { Grid, Typography, Button, Paper, IconButton, TextField } from '@mui/material';
+import { Grid, Typography, Button } from '@mui/material';
 import { FaUser } from "react-icons/fa";
-import { IoSettings } from "react-icons/io5";
-import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { RiEditLine } from "react-icons/ri";
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { useDispatch } from 'react-redux';
-import { userSuccess } from '../redux/reducers/user';
 import { FaIdCard } from "react-icons/fa6";
 import { FaCarAlt } from "react-icons/fa";
 import { FaTaxi } from "react-icons/fa6";
+import CustomTable from '../custom/CustomTable';
+let BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
 
 const Admin = () => {
 
-    const [settings, setSettings] = React.useState(false);
-    const [showPassword, setShowPassword] = React.useState(false);
-
+    const [activeTabs, setActiveTabs] = useState('users');
+    const [usersData, setUsersData] = useState([]);
+    const [driverData, setDriverData] = useState([]);
+    const [brandData, setBrandData] = useState([]);
+    const [cabData, setCabData] = useState([]);
     const userData = useSelector((state) => state.user);
-    console.log(userData);
-
-    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
 
 
+    const getUsers = useCallback( async () => {
+        try {
+            await fetch(`${BASE_URL}/cabzen/getAllUsers`, {
+
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "page": "1"
+                })
+
+            })
+                .then(response => response.json())
+
+                .then(data => {
+
+                    if (data.status === 'success') {
+                        setUsersData(data.data);
+                    }
+                    else {
+                        // alert(data.message);
+                        return;
+                    }
+
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                }
+                );
+
+        } catch (error) {
+            console.log(error);
+        }
+    },[token]);
+
+    const getDrivers = useCallback(async () => {
+        try {
+            await fetch(`${BASE_URL}/cabzen/getAllDrivers`, {
+
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "page": "1"
+                })
+
+            })
+
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 'success') {
+                        setDriverData(data.data);
+                    }
+                    else {
+                        // alert(data.message);
+                        return;
+                    }
+
+                }
+
+                )
+                .catch((error) => {
+                    console.error('Error:', error);
+                }
+                );
+
+        } catch (error) {
+            console.log(error);
+        }
+    },[
+        token
+    ],[token]);
+
+    const getBrands = useCallback(async () => {
+        try {
+            await fetch(`${BASE_URL}/cabzen/listAllBrands`, {
+
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "page": "1"
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+
+                    if (data.status === 'success') {
+                        setBrandData(data.data);
+                    }
+                    else {
+                        return;
+                    }
+                }
+                )
+                .catch((error) => {
+                    console.error('Error:', error);
+                }
+                );
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    },[token]);
+
+    const getCabs = useCallback(async () => {
+
+        try {
+            await fetch(`${BASE_URL}/cabzen/listAllCabs`, {
+
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    "page": "1"
+                })
+
+            })
+
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 'success') {
+                        setCabData(data.data);
+                    }
+                    else {
+                        return;
+                    }
+                }
+                )
+                .catch((error) => {
+                    console.error('Error:', error);
+                }
+
+                );
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    },[token]);
+
+    const renderTabContent = () => {
+        switch (activeTabs) {
+            case 'users':
+                return <CustomTable data={usersData} />;
+            case 'drivers':
+                return <CustomTable data={driverData} />;
+            case 'brands':
+                return <CustomTable data={brandData} />;
+            case 'cabs':
+                return <CustomTable data={cabData} />;
+            default:
+                return null;
+        }
+    };
+
+    useEffect(() => {
+        getUsers();
+        getDrivers();
+        getBrands();
+        getCabs();
+    }, [getBrands, getDrivers, getUsers, getCabs]);
 
     return (
         <>
@@ -34,9 +201,9 @@ const Admin = () => {
                 <Grid item xs={12} sm={3} style={{ backgroundColor: '#ffe6c1' }}>
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', marginTop: 60 }}>
                         <Typography variant='h5' style={{ color: 'black', fontWeight: "bold" }}>Admin Dashboard</Typography>
-                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px',marginTop: 5 }}>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '20px', marginTop: 5 }}>
                             <Typography variant='h6' style={{ color: 'black' }}>{userData?.data?.fullName}</Typography>
-                            <Typography  style={{ color: 'black', fontWeight:"bold" }}> ({userData?.data?.role && userData.data.role.toUpperCase()})</Typography>
+                            <Typography style={{ color: 'black', fontWeight: "bold" }}> ({userData?.data?.role && userData.data.role.toUpperCase()})</Typography>
                         </div>
                     </div>
 
@@ -44,33 +211,37 @@ const Admin = () => {
                     <div>
                         <div>
                             <Button style={{ width: '100%', textAlign: 'left', color: 'black' }}
-                                onClick={() => {
-                                    setSettings(false)
-                                }}
+                                onClick={() => setActiveTabs('users')}
                             >
                                 <FaUser style={{ marginRight: 10 }} />
-                               Users
+                                Users
                             </Button>
                         </div>
                         <hr color='orange' />
                         <div>
-                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }} onClick={() => setSettings(true)}>
-                            <FaIdCard style={{marginRight: 10}}/>
+                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }}
+                                onClick={() => setActiveTabs('drivers')}
+                            >
+                                <FaIdCard style={{ marginRight: 10 }} />
                                 Drivers
                             </Button>
                         </div>
                         <hr color='orange' />
                         <div>
-                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }} onClick={() => setSettings(true)}>
-                            <FaCarAlt style={{marginRight: 10}}/>
-                              Brands
+                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }}
+                                onClick={() => setActiveTabs('brands')}
+                            >
+                                <FaCarAlt style={{ marginRight: 10 }} />
+                                Brands
                             </Button>
                         </div>
                         <hr color='orange' />
                         <div>
-                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }} onClick={() => setSettings(true)}>
-                            <FaTaxi  style={{marginRight: 10}}/>
-                             Cabs
+                            <Button style={{ width: '100%', textAlign: 'left', color: 'black' }}
+                                onClick={() => setActiveTabs('cabs')}
+                            >
+                                <FaTaxi style={{ marginRight: 10 }} />
+                                Cabs
                             </Button>
                         </div>
                         <hr color='orange' />
@@ -78,7 +249,7 @@ const Admin = () => {
                 </Grid>
                 <Grid item xs={12} sm={9} style={{ backgroundColor: '#eeeeee' }}>
                     <div style={{ marginTop: 80, padding: 20 }}>
-
+                        {renderTabContent()}
                     </div>
                 </Grid>
             </Grid>
